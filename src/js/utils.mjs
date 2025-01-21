205a9fd5721230f8ba91d18515698bf420420d86
utahphotohunter <mitch.hunter1197@gmail.com>
2025-01-20 18:03:05 -0700

    changed getParam()

--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -24,8 +24,8 @@ export function setClick(selector, callback) {
 
 export function getParam(param){
   const queryString = window.location.search;
-const urlParams = new URLSearchParams(queryString);
-const product = urlParams.get('product')
+  const urlParams = new URLSearchParams(queryString);
+  const product = urlParams.get(`${param}`)
 
   return product
 }
