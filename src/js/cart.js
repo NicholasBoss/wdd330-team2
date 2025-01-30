e714cdbe79c47ce6d0cf533fb962ce98a866a79e
NicholasBoss <NicholasBoss0914@gmail.com>
2025-01-30 10:18:16 -0700

    fixed an image in tents.json and fixed the error handling on cart page

--- src/js/cart.js
+++ src/js/cart.js
@@ -2,6 +2,9 @@ import { getLocalStorage } from "./utils.mjs";
 
 function renderCartContents() {
   const cartItems = getLocalStorage("so-cart");
+  if (!cartItems) {
+    return;
+  }
   const htmlItems = cartItems.map((item) => cartItemTemplate(item));
   document.querySelector(".product-list").innerHTML = htmlItems.join("");
 }
